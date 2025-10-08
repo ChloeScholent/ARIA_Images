8b1b0bc100ded85b6110a6750ac222c626edbdb5
Chloé Scholent <cloviss@cloviss-1.home>
2025-10-08 11:37:13 +0200

    update

---
+++ code/load_model_with_mediapipe.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import mediapipe as mp
+import cv2
+from model_class import PowerliftingCNN
+
+print('Loading the model...')
+model = PowerliftingCNN(num_classes=3)
+model.load_state_dict(torch.load("Models/Powerlifting_CNN_Classification.pth", map_location="cpu"))
+model.eval()
+print('Model loaded !')
+
+
+# -----------------------
+# 2. MediaPipe Pose setup
+# -----------------------
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
+
+image_path = "powerlifting/train/images/deadlift_47_jpg.rf.497d03700d37bca33004bc112b2970e7.jpg"
+
+image = cv2.imread(image_path)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+results = pose.process(rgb)
+
+mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+cv2.imshow("Pose", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
